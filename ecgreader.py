d78f0643e9a96460b9940b2969374ff5096999da
Sara Alaa <sara3laa.93@gmail.com>
2018-08-26 07:36:00 +0200

    Read Signals and Annotation Online

--- ecgreader.py
+++ ecgreader.py
@@ -1,3 +1,11 @@
-import pandas as pd
+import wfdb
 
-records = pd.read_csv('records_names.csv', header=None, squeeze=True, index_col=0)
+
+def read_ecg_signal(record_name):
+    signals, fields = wfdb.rdsamp(record_name, sampfrom=0, sampto=None, channels=None, pb_dir='mitdb')
+    return signals, fields
+
+
+def read_ecg_ann(record_name):
+    annotation = wfdb.rdann(record_name, 'atr', pb_dir='mitdb')
+    return annotation.sample, annotation.symbol
